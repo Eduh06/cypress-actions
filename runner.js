30b7a06386e1f1e6f082a2a30e8d504ecf4c7999
Eduardo dos Santos <37030387+Eduh06@users.noreply.github.com>
2023-03-17 20:18:31 -0300

    Update runner.js

--- runner.js
+++ runner.js
@@ -1,7 +1,7 @@
 const cypress = require('cypress')      //importa o Cypress
 const tesults = require('cypress-tesults-reporter');  //importa o Tesults
 
-const TOKEN ='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjNhMWE5NzIyLTcwYTMtNGMwMi1iNWViLTI0MmNhNTljNDk3MS0xNjc4ODM4MzQzNTQxIiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiZTAxNTc2ZTctOGE3MS00OWVhLTkyZDMtYTk3ZDQ4ZGMxZWMyIiwidHlwZSI6InQifQ.B913iTedKZy9UwYn14pVgd2MqzS3m6b_V3tIeK8mWY8'
+const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjNhMWE5NzIyLTcwYTMtNGMwMi1iNWViLTI0MmNhNTljNDk3MS0xNjc4ODM4MzQzNTQxIiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiZTAxNTc2ZTctOGE3MS00OWVhLTkyZDMtYTk3ZDQ4ZGMxZWMyIiwidHlwZSI6InQifQ.B913iTedKZy9UwYn14pVgd2MqzS3m6b_V3tIeK8mWY8'
 
 cypress.run({
   // specs to run here
